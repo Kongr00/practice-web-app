91dc6f5521a3bc706a33f5189ea445d01820de2a
RenatKramarovskyi <vt222_krr@student.ztu.edu.ua>
2025-01-26 00:20:37 +0200

    feat: added posting comment logic from post page

--- fe/src/pages/concrete-post-page/PostPage.tsx
+++ fe/src/pages/concrete-post-page/PostPage.tsx
@@ -7,6 +7,10 @@ import {Post, Comment} from "../../types/types.ts";
 import {getToken} from "../../utils/getToken.ts";
 import CommentBlock from "./post-page-ui/CommentBlock/CommentBlock.tsx";
 import CommentList from "./post-page-ui/CommentList/CommentList.tsx";
+import {Simulate} from "react-dom/test-utils";
+import input = Simulate.input;
+import {Send} from "lucide-react";
+import { jwtDecode } from "jwt-decode";
 
 const PostPage = () => {
 
@@ -14,12 +18,13 @@ const PostPage = () => {
     const [post, setPost] = useState<Post>()
     const [comments, setComments] = useState<Comment[]>([])
 
+    const [commentInput, setCommentInput] = useState('')
 
     const [fetchPost, isPostLoading, fetchPostError] = useFetching( async () => {
             const postResponse = await axios.get<Post>(`/api/post/${id}`, {
                 headers: getToken()
             });
-            console.log(postResponse.data)
+            // console.log(postResponse.data)
             setPost(postResponse.data)
         }
     )
@@ -28,7 +33,7 @@ const PostPage = () => {
             const commentResponse = await axios.get<Comment[]>(`/api/comment/by-post-id/${id}`, {
                 headers: getToken()
             });
-            console.log(commentResponse.data)
+            // console.log(commentResponse.data)
             setComments(commentResponse.data)
         }
     )
@@ -37,34 +42,63 @@ const PostPage = () => {
         fetchComments()
     }, []);
 
+    const handleSendComment = async () => {
+        const decoded = jwtDecode(localStorage.getItem('token'));
 
+        const sendCommentResponse = await axios.post(`/api/comment`,
+            {
+                content: commentInput,
+                postId: id,
+                authorId: decoded.id
+            },
+            {
+                headers: getToken(),
+            }
+        );
+        fetchComments()
+        setCommentInput('')
+    }
 
     return (
         <div className={cls.container}>
-            <div className={cls.postArea}>
-                {
-                    isPostLoading
-                        ?  <div>Loading...</div>
-                        : <div>
-                            <div className={cls.postPhoto}>
-                                PHOTO
-                            </div>
-                            <div className={cls.postContent}>
-                                <div className={cls.title}>{post?.title}</div>
-                                <div className={cls.content}>{post?.content}</div>
-                                <div className={cls.category}>{post?.category}</div>
-                            </div>
+            <div className={cls.postContainer}>
+                <div className={cls.postArea}>
+                    {
+                        isPostLoading
+                            ?  <div>Loading...</div>
+                            : <div>
+                                <div className={cls.postPhoto}>
+                                    PHOTO
+                                </div>
+                                <div className={cls.postContent}>
+                                    <div className={cls.title}>{post?.title}</div>
+                                    <div className={cls.content}>{post?.content}</div>
+                                    <div className={cls.category}>{post?.category}</div>
+                                </div>
 
-                        </div>
-                }
+                            </div>
+                    }
+                </div>
+                <div className={cls.commentsListArea}>
+                    <CommentList
+                        comments={comments}
+                        isCommentsLoading={isCommentsLoading}
+                    />
+                </div>
             </div>
-            <div className={cls.commentsListArea}>
-                <CommentList
-                    comments={comments}
-                    isCommentsLoading={isCommentsLoading}
+            <div className={cls.inputContainer}>
+                <input
+                    type="text"
+                    value={commentInput}
+                    onChange={(e) =>
+                        setCommentInput(e.target.value)}
                 />
+                <button>
+                    <Send size={32} onClick={handleSendComment}/>
+                </button>
             </div>
         </div>
+
     );
 };
 
