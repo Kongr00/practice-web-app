2815e78f28923f85379359ee953569481e8b8f62
RenatKramarovskyi <vt222_krr@student.ztu.edu.ua>
2025-01-29 19:37:23 +0200

    feat: accidentally added some changes on profile pages, next changes will appear on feature/profile-page branch

--- fe/src/pages/profile-page/ProfilePage.tsx
+++ fe/src/pages/profile-page/ProfilePage.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import cls from './ProfilePage.module.css'
+import {useFetching} from "../../hooks/useFetchig.ts";
+import axios from "axios";
+import {Post} from "../../types/types.ts";
+import {getToken} from "../../utils/getToken.ts";
 const ProfilePage = () => {
+
+    const [userComments, setUserComments] = useState()
+
+    const {fetchComments, error, isLoading} = useFetching(async () => {
+        const postResponse = await axios.get<Post>(`/api`, {
+            headers: getToken()
+        });
+        setUserComments(postResponse.data)
+    })
+
+    useEffect(() => {
+        fetchComments()
+    }, []);
+
     return (
         <div className={cls.container}>
-            Profile Page
+            <div className={cls.profileBlockArea}>
+                <div className={cls.userInfoArea}>
+                    user info will be here
+                </div>
+                <div className={cls.commentsListArea}>
+                    user comments wil be here
+                </div>
+            </div>
         </div>
     );
 };
